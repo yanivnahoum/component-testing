3773f60f13b5308740d5e70d25ccdfe6234bfca5
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2023-06-21 09:24:58 +0300

    Exercise

---
+++ src/test/java/com/att/training/ct/exercises/DatabaseComponentTest.java
@@ -0,0 +1,9 @@
+/**
+ * Write 2 spring test classes that access a postgres db, initialized with the script in src/main/resources/db/init.sql
+ * They should use a singleton container, set to be reusable.
+ * Each of the 2 spring test classes should have a single test method.
+ * 1. UserDao.count(): already implemented
+ * 2. UserDao.getName(int id): Get the name of user with id=1
+ */
+package com.att.training.ct.exercises;
+
