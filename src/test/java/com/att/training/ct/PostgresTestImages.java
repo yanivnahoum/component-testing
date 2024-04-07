2a58b58e4075f616e491027124ffc14d96d8f424
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2024-04-07 16:25:58 +0300

    Upgraded to last 2.7.x, maven 3.9.6, postgresql 14.9

--- src/test/java/com/att/training/ct/PostgresTestImages.java
+++ src/test/java/com/att/training/ct/PostgresTestImages.java
@@ -3,5 +3,5 @@ package com.att.training.ct;
 import org.testcontainers.utility.DockerImageName;
 
 public class PostgresTestImages {
-    public static final DockerImageName DEFAULT_IMAGE = DockerImageName.parse("postgres:14.7");
+    public static final DockerImageName DEFAULT_IMAGE = DockerImageName.parse("postgres:14.9");
 }
