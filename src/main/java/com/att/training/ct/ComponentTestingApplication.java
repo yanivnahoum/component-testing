35b3a5e89efa42dcd35e9817bec8cb386f362ed3
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2024-10-21 11:53:55 +0300

    Upgraded Spring Boot and corrected and documented the requirements for flyway-database-postgresql

--- src/main/java/com/att/training/ct/ComponentTestingApplication.java
+++ src/main/java/com/att/training/ct/ComponentTestingApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ComponentTestingApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ComponentTestingApplication.class, args);
     }
-
 }
